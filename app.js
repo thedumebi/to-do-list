fb9238e16cdcb9c97e1bcf1678f2b5b6b08c5cec
Dumebi <chiwuzohdumebi@gmail.com>
2020-11-29 00:01:22 +0100

    gitignore update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const date = require(__dirname + "/date.js")
+const date = require(__dirname + "/date.js");
 let ejs = require('ejs');
 
 
